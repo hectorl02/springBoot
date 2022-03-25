6c8ae7947a1ae440f8b6d8252f19926c41115725
Hector Lopez <hectorlopezp02@gmail.com>
2022-03-25 14:07:46 -0500

    values at properties with pojo

--- src/main/java/com/hectorl/fundamentos/FundamentosApplication.java
+++ src/main/java/com/hectorl/fundamentos/FundamentosApplication.java
@@ -4,6 +4,7 @@ import com.hectorl.fundamentos.bean.MyBean;
 import com.hectorl.fundamentos.bean.MyBeanProperties;
 import com.hectorl.fundamentos.bean.MyBeanWithDependency;
 import com.hectorl.fundamentos.component.ComponentDependency;
+import com.hectorl.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,17 +17,20 @@ public class FundamentosApplication implements CommandLineRunner {
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanProperties myBeanProperties;
+	private UserPojo userPojo;
 
 	public FundamentosApplication(
 			@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
 			MyBean myBean , MyBeanWithDependency myBeanWithDependency,
-			MyBeanProperties myBeanProperties
+			MyBeanProperties myBeanProperties,
+			UserPojo userPojo
 	){
 		this.componentDependency = componentDependency;
 		//inyecta dependecia
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanProperties = myBeanProperties;
+		this.userPojo = userPojo;
 	}
 
 	public static void main(String[] args) {
@@ -40,6 +44,7 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanProperties.function());
+		System.out.println(userPojo.getEmail()+"--"+userPojo.getPassword());
 
 	}
 }
--- src/main/java/com/hectorl/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/hectorl/fundamentos/configuration/GeneralConfiguration.java
@@ -2,11 +2,14 @@ package com.hectorl.fundamentos.configuration;
 
 import com.hectorl.fundamentos.bean.MyBeanProperties;
 import com.hectorl.fundamentos.bean.MyBeanPropertiesImplement;
+import com.hectorl.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
