f409123607c158c861a6545029f1b5c178177de8
Hector Lopez <hectorlopezp02@gmail.com>
2022-03-25 23:26:36 -0500

    configure datasource

--- src/main/java/com/hectorl/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/hectorl/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,12 @@ import com.hectorl.fundamentos.bean.MyBeanPropertiesImplement;
 import com.hectorl.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.management.MXBean;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)
@@ -21,6 +25,16 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanProperties function(){
         return new MyBeanPropertiesImplement(name,apellido);
+    }
+
+    @Bean
+    public DataSource dataSource () {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("hector");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 
 }
