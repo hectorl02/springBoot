9611d6dcd862bad88ee47265d7feff18e6402062
Hector Lopez <hectorlopezp02@gmail.com>
2022-03-30 00:32:27 -0500

    paginacion user

--- src/main/java/com/hectorl/fundamentos/controller/UserRestController.java
+++ src/main/java/com/hectorl/fundamentos/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.hectorl.fundamentos.caseuse.DeleteUser;
 import com.hectorl.fundamentos.caseuse.GetUser;
 import com.hectorl.fundamentos.caseuse.UpdateUser;
 import com.hectorl.fundamentos.entity.User;
+import com.hectorl.fundamentos.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,17 +21,20 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
     public UserRestController(
             GetUser getUser,
             CreateUser createUser,
             DeleteUser deleteUser,
-            UpdateUser updateUser
+            UpdateUser updateUser,
+            UserRepository userRepository
     ) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -51,6 +56,11 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id) {
         return  new ResponseEntity<>(updateUser.update(newUser, id),HttpStatus.OK);
+    }
+
+    @GetMapping("/pageable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size){
+        return userRepository.findAll(PageRequest.of(page, size)).getContent();
     }
 
 
